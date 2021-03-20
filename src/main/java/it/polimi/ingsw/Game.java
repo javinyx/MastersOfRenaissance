a61f96bdbc4e49b3f5ca26483c5090d59d0e7c8e
Ottavia <ottavia.belotti@mail.polimi.it>
2021-03-20 15:39:12 +0100

    Game and Player class added

---
+++ src/main/java/it/polimi/ingsw/Game.java
@@ -0,0 +1,23 @@
+package it.polimi.ingsw;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Game {
+    private Player currPlayer;
+    private List<Player> players;
+    private int totalPlayers;
+
+    public Game(){
+        players = new ArrayList<>();
+        totalPlayers = 0;
+        currPlayer = null;
+    }
+    public void createPlayer(String nickname){
+        if(totalPlayers==4){
+            //lanciare eccezione tooManyPlayers
+        }
+        totalPlayers++;
+        players.add(new Player(nickname, totalPlayers));
+    }
+}
---
+++ src/main/java/it/polimi/ingsw/Player.java
@@ -0,0 +1,8 @@
+package it.polimi.ingsw;
+
+public class Player {
+
+    public Player(String nickname, int turnID){
+
+    }
+}

b1c7f5b2a71567bfbe0cf8579c6b2b062923de6f
AlessioBraccini <alessio.braccini@mail.polimi.it>
2021-04-14 20:39:21 +0200

    Add 2 test classes

---
+++ src/test/java/it/polimi/ingsw/model/cards/DeckTest.java
@@ -0,0 +1,24 @@
+package it.polimi.ingsw.model.cards;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DeckTest {
+
+    @BeforeEach
+    public void testSetup(){
+
+        Deck d = new Deck("ActionToken");
+
+    }
+
+    @Test
+    void getCards() {
+    }
+
+    @Test
+    void createProdDeckList() {
+    }
+}
---
+++ src/test/java/it/polimi/ingsw/model/cards/actiontoken/DiscardTokenTest.java
@@ -0,0 +1,18 @@
+package it.polimi.ingsw.model.cards.actiontoken;
+
+import it.polimi.ingsw.model.cards.Deck;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DiscardTokenTest {
+
+    @Test
+    void draw() {
+
+        Deck d = new Deck("ActionToken");
+
+        //System.out.println(d);
+
+    }
+}

8afd7a88eb7c32481b27d032da19b1fa77948cd2
Javinyx <javinbarone@gmail.com>
2021-04-24 17:27:45 +0200

    add: temporary test case

--- src/test/java/it/polimi/ingsw/model/stub/SingleGameStub.java
+++ src/test/java/it/polimi/ingsw/model/stub/SingleGameStub.java
@@ -1,8 +1,23 @@
 package it.polimi.ingsw.model.stub;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import it.polimi.ingsw.MastersOfRenaissance;
 import it.polimi.ingsw.model.SinglePlayerGame;
+import it.polimi.ingsw.model.cards.Card;
+import it.polimi.ingsw.model.cards.Deck;
+import it.polimi.ingsw.model.cards.leader.LeaderCard;
+import it.polimi.ingsw.model.cards.leader.MarbleAbility;
 import it.polimi.ingsw.model.cards.production.ConcreteProductionCard;
 import it.polimi.ingsw.model.player.ProPlayer;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 public class SingleGameStub extends SinglePlayerGame {
 
@@ -17,4 +32,23 @@ public class SingleGameStub extends SinglePlayerGame {
     public void setCurrPlayer(ProPlayer p){
         currPlayer = p;
     }
-}
+
+    public LeaderCard createMarbleCard(){
+        ArrayList<LeaderCard> cardList = new ArrayList<>();
+        ArrayDeque<LeaderCard> cardDeque;
+        LeaderCard marbleAbilityCard;
+
+        Gson gson = new Gson();
+
+        try (Reader reader = new InputStreamReader(Objects.requireNonNull(MastersOfRenaissance.class.getResourceAsStream("/json/LeaderCards/MarbleAbilityCards.json")))) {
+            cardList.addAll(gson.fromJson(reader, new TypeToken<ArrayList<MarbleAbility>>(){}.getType()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        cardDeque = new ArrayDeque<>(cardList);
+        marbleAbilityCard = cardDeque.peekFirst();
+
+        return marbleAbilityCard;
+    }
+    }

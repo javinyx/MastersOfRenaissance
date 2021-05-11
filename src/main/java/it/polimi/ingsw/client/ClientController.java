ce51b531d0fbb23b355337743c09eb974b2594c2
Ottavia <ottavia.belotti@mail.polimi.it>
2021-05-11 20:30:18 +0200

    add: client updateAction

--- src/main/java/it/polimi/ingsw/client/ClientController.java
+++ src/main/java/it/polimi/ingsw/client/ClientController.java
@@ -1,28 +1,62 @@
 package it.polimi.ingsw.client;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import it.polimi.ingsw.MastersOfRenaissance;
+import it.polimi.ingsw.client.model.Market;
 import it.polimi.ingsw.client.model.NubPlayer;
 import it.polimi.ingsw.messages.concreteMessages.ChooseLeaderCardsMessage;
 import it.polimi.ingsw.messages.concreteMessages.ChoosePlacementsInStorageMessage;
 import it.polimi.ingsw.messages.concreteMessages.UpdateMessage;
-import it.polimi.ingsw.model.cards.leader.LeaderCard;
+import it.polimi.ingsw.model.cards.leader.*;
 import it.polimi.ingsw.model.cards.production.ConcreteProductionCard;
-import it.polimi.ingsw.model.player.Player;
-import it.polimi.ingsw.model.player.ProPlayer;
+import it.polimi.ingsw.model.market.Resource;
 
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class ClientController {
     private ViewInterface view;
     private boolean myTurn;
+    private List<ConcreteProductionCard> allProductionCards;
+    private List<LeaderCard> allLeaders;
     private List<LeaderCard> leaders = new ArrayList<>();
     private List<ConcreteProductionCard> availableProductionCard = new ArrayList<>();
-    private List<NubPlayer> players = new ArrayList<>();
+    private List<NubPlayer> otherPlayers = new ArrayList<>();
     private NubPlayer player;
+    private Market market;
+
+    private static final String prodPath = "/json/ProductionCards.json",
+            leadDiscountPath = "/json/LeaderCards/DiscountAbilityCards.json",
+            leadStoragePath = "/json/LeaderCards/StorageAbilityCards.json",
+            leadMarblePath = "/json/LeaderCards/MarbleAbilityCards.json",
+            leadBoostPath = "/json/LeaderCards/BoostAbilityCards.json",
+            tokenDiscardPath = "/json/ActionTokens/DiscardTokens.json",
+            tokenDoubleMovePath = "/json/ActionTokens/DoubleMoveTokens.json",
+            tokenMoveShufflePath = "/json/ActionTokens/MoveShuffleTokens.json";
 
     public ClientController(ViewInterface view, NubPlayer player){
         this.view = view;
         this.player = player;
+        initAllCards();
+    }
+
+    private void initAllCards(){
+        Gson gson = new Gson();
+        Reader reader = new InputStreamReader(Objects.requireNonNull(MastersOfRenaissance.class.getResourceAsStream(prodPath)));
+        allProductionCards = gson.fromJson(reader, new TypeToken<List<ConcreteProductionCard>>(){}.getType());
+        reader = new InputStreamReader(Objects.requireNonNull(MastersOfRenaissance.class.getResourceAsStream(leadDiscountPath)));
+        allLeaders.addAll(gson.fromJson(reader, new TypeToken<List<DiscountAbility>>(){}.getType()));
+        reader = new InputStreamReader(Objects.requireNonNull(MastersOfRenaissance.class.getResourceAsStream(leadStoragePath)));
+        allLeaders.addAll(gson.fromJson(reader, new TypeToken<List<StorageAbility>>(){}.getType()));
+        reader = new InputStreamReader(Objects.requireNonNull(MastersOfRenaissance.class.getResourceAsStream(leadMarblePath)));
+        allLeaders.addAll(gson.fromJson(reader, new TypeToken<List<MarbleAbility>>(){}.getType()));
+        reader = new InputStreamReader(Objects.requireNonNull(MastersOfRenaissance.class.getResourceAsStream(leadBoostPath)));
+        allLeaders.addAll(gson.fromJson(reader, new TypeToken<List<BoostAbility>>(){}.getType()));
     }
 
     public boolean getMyTurn(){return myTurn;}
@@ -30,11 +64,14 @@ public class ClientController {
     public void addLeaders(LeaderCard leader){leaders.add(leader);}
     public void removeLeader(LeaderCard leader){leaders.remove(leader);}
     public void setMyTurn(boolean status){this.myTurn = status;}
-    public void addPlayers(NubPlayer player){this.players.add(player);}
-    public List<NubPlayer> getPlayers(){return players;}
+    public void addPlayers(NubPlayer player){this.otherPlayers.add(player);}
+    public List<NubPlayer> getPlayers(){return otherPlayers;}
     public NubPlayer getPlayer(){return player;}
     public void initAvailableProductionCard(){}
 
+    public void setMarket(Resource[][] market, Resource extra){ this.market = new Market(market,extra);}
+
+    //---------------------------------ACTIONS: behaviours caused by Messages--------------------------
     public void chooseResourceAction(int quantity) {
         view.showMessage("Choose no." + quantity + " resources");
     }
@@ -46,13 +83,52 @@ public class ClientController {
     public void chooseLeadersAction(ChooseLeaderCardsMessage msg){
         view.showMessage("Choose no." + msg.getQuantity() + " of leaders among these:\n" + msg.getLeaders());
     }
-
-    public void updateAction(UpdateMessage msg){
-
-    }
-
     public void errorAction(String str){
         view.showMessage(str);
     }
+
+    public void updateAction(UpdateMessage msg){
+        if(market==null){
+            market = new Market(msg.getMarketBoard(), msg.getExtraMarble());
+        }else {
+            market.setMarketBoard(msg.getMarketBoard());
+            market.setExtra(msg.getExtraMarble());
+        }
+        view.updateMarket();
+
+        availableProductionCard = convertIdToProductionCard(msg.getAvailableProductionCards());
+        view.updateAvailableProductionCards();
+
+        ConcreteProductionCard boughtProductionCard = convertIdToProductionCard(msg.getProductionCardId().getT());
+        for(NubPlayer pp : otherPlayers){
+            if(pp.getTurnID()==msg.getPlayerId()){
+                pp.setCurrPos(msg.getPlayerPos());
+                pp.addProductionCard(boughtProductionCard, msg.getProductionCardId().getV()-1);
+                pp.setLeaders(convertIdToLeaderCard(msg.getLeadersId()));
+                pp.setAllResources(msg.getResources());
+                view.updateOtherPlayer(pp);
+                break;
+            }
+        }
+
+    }
+
+    //---------------------------CONVERTERS---------------------------------
+    private List<ConcreteProductionCard> convertIdToProductionCard(List<Integer> ids){
+        return allProductionCards.stream().filter(x -> ids.contains(x.getId())).collect(Collectors.toList());
+    }
+    private ConcreteProductionCard convertIdToProductionCard(int id){
+        for(ConcreteProductionCard pc : allProductionCards){
+            if(pc.getId()==id)
+                return pc;
+        }
+        return null;
+    }
+
+    private List<LeaderCard> convertIdToLeaderCard(List<Integer> ids){
+        return allLeaders.stream().filter(x -> ids.contains(x.getId())).collect(Collectors.toList());
+    }
+
+
 
 }
--- src/main/java/it/polimi/ingsw/client/ViewInterface.java
+++ src/main/java/it/polimi/ingsw/client/ViewInterface.java
@@ -1,6 +1,7 @@
 package it.polimi.ingsw.client;
 
 import com.google.gson.Gson;
+import it.polimi.ingsw.client.model.NubPlayer;
 
 public abstract class ViewInterface{
     //tutti i metodi astratti che ci sarà bisogno di implementare nella cli e gui, ex: showInfo, showError, showBlaBla
@@ -15,4 +16,9 @@ public abstract class ViewInterface{
 
     //----------------------SHOW MESSAGE-------------------
     public abstract void showMessage(String str);
+
+    public abstract void updateMarket();
+    public abstract void updateAvailableProductionCards();
+    public abstract void updateThisPlayer();
+    public abstract void updateOtherPlayer(NubPlayer player);
 }
